2477614bcc737d1df64924eaf15707e7239895d1
thespeace <thespeace1@gmail.com>
2024-05-26 20:15:01 +0900

    TEST: Spring transaction propagation(basic) - Transaction used twice

--- src/test/java/thespeace/springtx/propagation/BasicTxTest.java
+++ src/test/java/thespeace/springtx/propagation/BasicTxTest.java
@@ -64,4 +64,58 @@ public class BasicTxTest {
         txManager.rollback(status);
         log.info("트랜잭션 롤백 완료");
     }
+
+    /**
+     * <h1>스프링 트랜잭션 전파2 - 트랜잭션 두 번 사용</h1>
+     * 로그를 보면 트랜잭션1과 트랜잭션2가 같은 conn0 커넥션을 사용중이다. 이것은 중간에 커넥션 풀 때문에 그런 것이다.
+     * 트랜잭션1은 conn0 커넥션을 모두 사용하고 커넥션 풀에 반납까지 완료했다. 이후에 트랜잭션2가 conn0 를 커넥션
+     * 풀에서 획득한 것이다. 따라서 둘은 완전히 다른 커넥션으로 인지하는 것이 맞다.<p><p>
+     *
+     * 그렇다면 둘을 구분할 수 있는 다른 방법은 없을까?<p><p>
+     *
+     * 히카리 커넥션 풀에서 커넥션을 획득하면 실제 커넥션을 그대로 반환하는 것이 아니라 내부 관리를 위해 히카리 프록시
+     * 커넥션이라는 객체를 생성해서 반환한다. 물론 내부에는 실제 커넥션이 포함되어 있다. 이 객체의 주소를 확인하면 커넥션
+     * 풀에서 획득한 커넥션을 구분할 수 있다.<p><p>
+     *
+     * <ul>
+     *     <li>트랜잭션1: Acquired Connection [HikariProxyConnection@1000000 wrapping conn0]</li>
+     *     <li>트랜잭션2: Acquired Connection [HikariProxyConnection@2000000 wrapping conn0]</li>
+     * </ul>
+     *
+     * 히카리 커넥션풀이 반환해주는 커넥션을 다루는 프록시 객체의 주소가 트랜잭션1은
+     * HikariProxyConnection@1000000 이고, 트랜잭션2는 HikariProxyConnection@2000000 으로 서로 다
+     * 른 것을 확인할 수 있다.<br>
+     * 결과적으로 conn0 을 통해 커넥션이 재사용 된 것을 확인할 수 있고, HikariProxyConnection@1000000,
+     * HikariProxyConnection@2000000 을 통해 각각 커넥션 풀에서 커넥션을 조회한 것을 확인할 수 있다.
+     */
+    @Test
+    void double_commit() {
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋");
+        txManager.commit(tx1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 커밋");
+        txManager.commit(tx2);
+    }
+
+    /**
+     * 커밋과 롤백 트랜잭션을 사용해도 위와 마찬가지로 결국 다른 커넥션을 쓴다.<p><p>
+     *
+     * 그렇다면 트랜잭션을 이미 하고 있는데 그 안에서 또 트랜잭션이 발생하면 어떻게 될까?
+     */
+    @Test
+    void double_commit_rollback() {
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋");
+        txManager.commit(tx1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 롤백");
+        txManager.rollback(tx2);
+    }
 }
